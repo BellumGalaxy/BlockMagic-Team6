5f29aa46edb502b2af7e954c8972f9d64ca4130d
Barba <patrickamc@protonmail.com>
2024-06-02 20:52:18 -0300

    Smart Contract - waiting API Definition

--- SmartContracts/src/SBFunctions.sol
+++ SmartContracts/src/SBFunctions.sol
@@ -37,17 +37,14 @@ contract SBFunctions is FunctionsClient, Ownable{
     }
     
     struct GamesToBet{
-        uint256 homeTeam;
-        uint256 awayTeam;
-        uint256 homeOdds;
-        uint256 awayOdds;
-        uint256 drawOdds;
-        uint256 teamWinner;
+        string fixture;
+        string winnerTeam;
     }
 
     struct SmartBet{
         uint256 betDate;
-        uint256 team;
+        uint256 matchId;
+        string team;
         uint256 amountBet;
         uint256 possibleEarnings;
         BetStatus status;
@@ -63,9 +60,9 @@ contract SBFunctions is FunctionsClient, Ownable{
     /////////////
     ///Storage///
     /////////////
-    mapping(uint256 betId => GamesToBet) public s_games;
+    mapping(uint256 matchId => GamesToBet) public s_games;
     mapping(uint256 betId => address[] bettors) public s_bettors;
-    mapping(address user => mapping(uint256 betId => SmartBet[])) public s_smartBet;
+    mapping(address user => mapping(uint256 betId => SmartBet)) public s_smartBet;
     mapping(bytes32 requestId => FunctionsResponse) public s_functionsRequest;
 
     ///////////////
@@ -75,17 +72,17 @@ contract SBFunctions is FunctionsClient, Ownable{
 
     ///@notice Java Script code to interact with API
     string private constant GET =
-        "const gameName = args[0];"
         "const response = await Functions.makeHttpRequest({"
-        "url: `http://endereco/score/name/${}`,"
+        "url: `https://api.sportmonks.com/v3/football/fixtures?api_token=NxfugpPu4lKerTnpocWXqjBg0QiKcz2XLLgeuQ25Du1pYMYdEqGDT7EmJNCe`,"
         "method: 'GET',"
         "});"
         "if (response.error) {"
         "  throw Error(`Request failed message ${response.message}`);"
         "}"
         "const { data } = response;"
-        "return Functions.encodeUint256(data.score);"
+        "return Functions.encodeString(data);"
     ;
+
     ///@notice Magic Number Removal
     uint256 private constant ONE = 1;
     ///@notice the token accepted for payments
@@ -100,6 +97,7 @@ contract SBFunctions is FunctionsClient, Ownable{
     ////////////
     ///Events///
     ////////////
+    event SBFunctions_BetCreated(uint256 matchId);
     event SBFunctions_NewBetCreated(uint256 betId, address bettor, uint256 amountBet);
     event SBFunctions_Response(bytes32 indexed requestId, bytes response, bytes err);
 
@@ -135,76 +133,74 @@ contract SBFunctions is FunctionsClient, Ownable{
     //////////////
     ///external///
     //////////////
+    /**
+     * @notice Function to create new bets
+     * @param _matchId the Id of the match
+     * @param _fixture the team of the fixture team
+     * @param _teamWinner the winner team
+     */
+    function createBet(
+        uint256 _matchId,
+        string memory _fixture,
+        string memory _teamWinner
+    ) external onlyOwner {
+        s_games[_matchId] = GamesToBet({
+            fixture: _fixture,
+            winnerTeam: _teamWinner
+        });
+
+        emit SBFunctions_BetCreated(_matchId);
+    }
+
     function bet(
-        uint256 _betId, 
-        uint256 _team, 
+        uint256 _matchId,
+        string memory _team, 
         uint256 _amountBet, 
         uint256 _possibleEarnings
-    ) external {
+    ) external returns(uint256 betId){
         SmartBet memory smartBet = SmartBet({
             betDate: block.timestamp,
+            matchId: _matchId,
             team: _team,
             amountBet: _amountBet,
             possibleEarnings: _possibleEarnings,
             status: BetStatus.Ongoing
         });
 
-        s_smartBet[msg.sender][_betId].push(smartBet);
-        s_bettors[_betId].push(msg.sender);
+        s_smartBet[msg.sender][++s_betId] = (smartBet);
+        s_bettors[s_betId].push(msg.sender);
 
-        emit SBFunctions_NewBetCreated(_betId, msg.sender, _amountBet);
+        betId = s_betId;
+
+        emit SBFunctions_NewBetCreated(s_betId, msg.sender, _amountBet);
+
+        IERC20(i_bet).safeTransferFrom(msg.sender, address(this), _amountBet);
     }
 
+    /**
+     * @notice function for user to withdraw
+     * @param _betId Id of the bet to withdraw
+     */
     function winnerWithdraw(uint256 _betId) external {
-        uint256 betsNumber = s_smartBet[msg.sender][_betId].length;
-        if(betsNumber < ONE) revert SBFunctions_YouDontHaveAnyBetInThisGame();
-
-        bool isWinner;
-        uint256 index;
-
-        if(betsNumber > ONE){
-            for(uint i; i < betsNumber; ++i){
-                SmartBet memory smartBet = s_smartBet[msg.sender][_betId][i];
-                GamesToBet memory game = s_games[_betId];
-                
-                if(smartBet.team == game.teamWinner){
-                    isWinner = true;
-                    index = i;
-                    break;
-                }
-            }
-        } else {
-            SmartBet memory smartBet = s_smartBet[msg.sender][_betId][0];
-            GamesToBet memory game = s_games[_betId];
-
-            if(smartBet.team == game.teamWinner){
-                smartBet.team == game.teamWinner;
-                isWinner = true;
-            }
-        }
-
-        if(isWinner == true){
-            s_smartBet[msg.sender][_betId][index].status = BetStatus.Winner;
-            IERC20(i_bet).safeTransfer(msg.sender, s_smartBet[msg.sender][_betId][index].possibleEarnings);
+        SmartBet storage smartBet = s_smartBet[msg.sender][_betId];
+        GamesToBet memory game = s_games[smartBet.matchId];
+        
+        if(keccak256(abi.encode(smartBet.team)) == keccak256(abi.encode(game.winnerTeam))){
+            smartBet.status = BetStatus.Winner;
+            IERC20(i_bet).safeTransfer(msg.sender, s_smartBet[msg.sender][_betId].possibleEarnings);
         }
     }
 
     /**
      * @notice Sends an HTTP request for character information
-     * @param _args The arguments to pass to the HTTP request
      * @return requestId The ID of the request
     */
     function sendRequestToGet(
-        string[] memory _args
     ) external onlyOwner returns(bytes32 requestId) {
-        if(_args.length < ONE) revert SBFunctions_EmptyArgs();
 
         FunctionsRequest.Request memory req;
         // Initialize the request with JS code
         req.initializeRequestForInlineJavaScript(GET);
-
-        // Set the arguments for the request
-        req.setArgs(_args);
 
         // Send the request and store the request ID
         requestId = _sendRequest(
@@ -222,10 +218,6 @@ contract SBFunctions is FunctionsClient, Ownable{
         });
     }
 
-    ////////////
-    ///public///
-    ////////////
-
     //////////////
     ///internal///
     //////////////
@@ -242,22 +234,15 @@ contract SBFunctions is FunctionsClient, Ownable{
         request.lastError = _err;
 
         (
-            uint256 homeTeam, 
-            uint256 awayTeam, 
-            uint256 homeOdds, 
-            uint256 awayOdds, 
-            uint256 drawOdds,
-            uint256 teamWinner
-        ) = abi.decode(_response, (uint256,uint256,uint256,uint256,uint256,uint256));
+            uint256 matchId, 
+            string memory fixture,
+            string memory teamWinner
+        ) = abi.decode(_response, (uint256,string,string));
 
-        s_games[request.betId] = GamesToBet ({
-            homeTeam: homeTeam,
-            awayTeam: awayTeam,
-            homeOdds: homeOdds,
-            awayOdds: awayOdds,
-            drawOdds: drawOdds,
-            teamWinner: teamWinner
-        });
+        GamesToBet storage game = s_games[matchId];
+        
+        game.fixture = fixture;
+        game.winnerTeam = teamWinner;
 
         emit SBFunctions_Response(_requestId, _response, _err);
     }
